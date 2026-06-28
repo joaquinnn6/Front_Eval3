7022bf84562a1bf34f207a3d64faaa0974681cba
joaquinnn6 <joaqu.contrerasb@duocuc.cl>
2026-06-28 03:06:10 -0400

    fix

--- src/main/java/com/eval3/frontend/StaticPageGenerator.java
+++ src/main/java/com/eval3/frontend/StaticPageGenerator.java
@@ -398,8 +398,8 @@ footer {
     }
 
     private static void generateScriptJs() throws IOException {
-        String usersUrl = getEnv("BACKEND_USERS_URL=http://100.54.249.91");
-        String productsUrl = getEnv("BACKEND_PRODUCTS_URL=http://100.54.249.91");
+        String usersUrl = getEnv("BACKEND_USERS_URL", "http://localhost:8081");
+        String productsUrl = getEnv("BACKEND_PRODUCTS_URL", "http://localhost:8082");
         System.out.println("Configurando frontend: Users API -> " + usersUrl + " | Products API -> " + productsUrl);
 
         String js = """
